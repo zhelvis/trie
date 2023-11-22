da7515ec4dbe2e3b6f3987600ebdf90d31d0da64
vladimir zhelvis <zhelvisvladimir@gmail.com>
2023-11-22 21:02:35 +0300

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,15 @@ import { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
+  coveragePathIgnorePatterns: ["/node_modules/", "/helpers/"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
 
 export default config;
