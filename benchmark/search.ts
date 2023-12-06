f60b3b695ce2df567863eabc081d42289facf81b
Vladimir Zhelvis <v.zhelvis@adguard.com>
2023-12-06 19:36:55 +0300

    update search benchmark

--- benchmark/search.ts
+++ benchmark/search.ts
@@ -1,30 +1,41 @@
 import { createRandomStringArray, getAverageValue, getRandomElement } from "../helpers/array";
 import { getRandomString } from "../helpers/string";
 import { Trie } from "../src/trie";
+import { LabelledTree } from "../src/succinct/labelled-tree";
 
 const ROUNDS = 10_000;
 
 const MIN_SIZE_EXPONENT = 2;
 const MAX_SIZE_EXPONENT = 5;
 
-const res: { [size: number]: number } = {};
+const res: { [structure: string]: { [size: number]: number } } = {};
 
 for (let i = MIN_SIZE_EXPONENT; i <= MAX_SIZE_EXPONENT; i++) {
   const size = 10 ** i;
   const data = createRandomStringArray(size);
   const trie = Trie.create(data);
+  const labelledTree = LabelledTree.create(trie.root);
 
-  const mesurements: number[] = [];
+  const trieMeasurements: number[] = [];
+  const labelledTreeMeasurements: number[] = [];
 
   for (let j = 0; j < ROUNDS; j++) {
     const word = j % 2 ? getRandomElement(data) : getRandomString();
 
-    const start = performance.now();
+    let start = performance.now();
     trie.search(word);
-    mesurements.push(performance.now() - start);
+    trieMeasurements.push(performance.now() - start);
+
+
+    start = performance.now();
+    labelledTree.search(word);
+    labelledTreeMeasurements.push(performance.now() - start);
   }
 
-  res[size] = getAverageValue(mesurements);
+  res.trie = {};
+  res["labelled tree"] = {};
+  res.trie[size] = getAverageValue(trieMeasurements);
+  res["labelled tree"][size] = getAverageValue(labelledTreeMeasurements);
 }
 
 console.table(res);
